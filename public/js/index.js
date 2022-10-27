ea370a16251cf090f9df4d5531079062eee0a4b1
unknown <zcfshr@gmail.com>
2022-10-26 20:57:44 -0600

    added code for index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,29 @@
 
+const searchButton = document.getElementById('button-addon2');
 const apiKey = 'ac0d6fdd42e9490c9186a4a13ee7ab15';
-let searchedIngredient = [];
-const spoonacularURL = 'https://api.spoonacular.com/recipes/findByIngredients?' + apiKey + '&ingredients=' + searchedIngredient[0];
+//let searchedIngredient = [];
+const ingredientInput = document.getElementById('floatingInput').value;
+const spoonacularURL = 'https://api.spoonacular.com/recipes/findByIngredients?' + apiKey + '&ingredients=' + ingredientInput;
+console.log(spoonacularURL);
+
+//todo: event listener for button
+searchButton.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    //todo: fetch api data
+    fetch(spoonacularURL) //calls api
+    .then(function (response) {
+    if (response.status === '404') {
+        console.log(err);
+    } else {
+        return response.json();
+    }
+    })
+    .then(function (data) {
+    console.log(data); //console logs data from api
+    
+});
+})
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const { home } = require('../controllers/homeController');
 router.get('/', home);
 //router.use('/api', api);
 
+const recipeRoutes = require('./api/recipe-routes');
+router.use('./recipe-routes', recipeRoutes);
+
 module.exports = router;
