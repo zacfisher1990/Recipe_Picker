c88074ae1ea642ca216d0bf94442c51627e6d83a
unknown <zcfshr@gmail.com>
2022-10-26 19:26:19 -0600

    started code for api

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+
+const apiKey = 'ac0d6fdd42e9490c9186a4a13ee7ab15';
+let searchedIngredient = [];
+const spoonacularURL = 'https://api.spoonacular.com/recipes/findByIngredients?' + apiKey + '&ingredients=' + searchedIngredient[0];
+
