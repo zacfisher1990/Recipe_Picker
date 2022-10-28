2120935d521c33a010b9656d8121bb190f7ec1b2
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-10-27 20:04:31 -0600

    trying to fix the tab issue and pulling from main

--- public/js/index.js
+++ public/js/index.js
@@ -26,4 +26,24 @@ searchButton.addEventListener('click', function(event){
 })
 
 
+// homepage tabs js
 
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) =>{
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const showTab = tab.dataset.target;
+        console.log(target);
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === showTab) {
+                box.classList.remove('is-hidden');
+            } else {
+                box.classList.add('is-hidden');
+            }
+        });
+    });
+});
